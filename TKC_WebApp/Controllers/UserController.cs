d5e5ac46d0e48339462d136bc4ed8679a9ec071d
bxstar <yurongsheng@163.com>
2014-11-10 08:23:27 +0800

    UserController测试前提交

--- TKC_WebApp/Controllers/UserController.cs
+++ TKC_WebApp/Controllers/UserController.cs
@@ -58,31 +58,31 @@ namespace TKC_WebApp.Controllers
                 }
                 // 定义User对象
                 EntityUser session = new EntityUser();
-                session.UserName = UserName;
-                session.TopSessions = topSession;
-                session.SubUserName = UserName;
-                session.LoginUrl = string.Format("top_appkey={0}&top_parameters={1}&top_session={2}&top_sign={3}", Request["top_appkey"], HttpUtility.UrlEncode(Request["top_parameters"]), Request["top_session"], Request["top_sign"]);
+                session.fUserName = UserName;
+                session.fSession = topSession;
+                session.fSubUserName = UserName;
+                session.fLoginUrl = string.Format("top_appkey={0}&top_parameters={1}&top_session={2}&top_sign={3}", Request["top_appkey"], HttpUtility.UrlEncode(Request["top_parameters"]), Request["top_session"], Request["top_sign"]);
                 string message = string.Empty;
                 string ips = Request.ServerVariables.Get("Remote_Addr").ToString();
 
                 string userSubscribe = bllUserHandler.GetUserVas(session, ref message);
                 if (userSubscribe.Split('|').Length == 2)
                 {
-                    logger.InfoFormat("用户：{0}，订购信息{1}", session.SubUserName, userSubscribe);
+                    logger.InfoFormat("用户：{0}，订购信息{1}", session.fSubUserName, userSubscribe);
                     session.FeeCode = userSubscribe.Split('|')[0];
                     session.DeadLine = userSubscribe.Split('|')[1];
                 }
 
                 Session["user"] = session;
 
-                Boolean isVipUser = bllUserHandler.CheckUserVip(session.UserName);
+                Boolean isVipUser = bllUserHandler.CheckUserVip(session.fUserName);
                 if (isVipUser && Request.Url.ToString().ToLower().Contains("tkc.taokuaiche.com"))
                 {//是VIP用户，并且请求的是淘快词的Url，则跳转到测试网站
-                    string vipUrl = string.Format("http://vip.taokuaiche.com/login?{0}", session.LoginUrl);
+                    string vipUrl = string.Format("http://vip.taokuaiche.com/login?{0}", session.fLoginUrl);
                     return Redirect(vipUrl);
                 }
 
-                if (session.FeeCode == "ts-25420-1" || bllUserHandler.IsUserTestBasicVersion(session.UserName))
+                if (session.FeeCode == "ts-25420-1" || bllUserHandler.IsUserTestBasicVersion(session.fUserName))
                 {//初级版
                     var ip = "127.0.0.1";
                     if (Request.ServerVariables.Get("Remote_Addr").ToString() != String.Empty)
@@ -91,15 +91,15 @@ namespace TKC_WebApp.Controllers
                     }
                     // 数据库中取出用户信息
                     var param = new Dictionary<string, object>();
-                    param.Add("userName", session.UserName);
-                    param.Add("subUserName", session.SubUserName);
+                    param.Add("userName", session.fUserName);
+                    param.Add("subUserName", session.fSubUserName);
                     param.Add("userId", "");
                     param.Add("isPoxy", "1");
-                    param.Add("session", session.TopSessions);
+                    param.Add("session", session.fSession);
                     param.Add("Ip", ip);
                     param.Add("Versions", "1.0");
                     //主账户使用淘宝返回的地址参数更新数据库，代理账户需要从数据库中取得loginUrl，所以要使该值的最终值有效，被代理用户必须先于代理用户登录，该值在数据库中才存在，该值的做法类似userId
-                    param.Add("loginUrl", session.LoginUrl);
+                    param.Add("loginUrl", session.fLoginUrl);
                     param.Add("FeeCode", session.FeeCode);
                     // 获取用户信息
                     session = bllUserHandler.UpdateUserInfo(param, true);
@@ -122,23 +122,23 @@ namespace TKC_WebApp.Controllers
                         ip = Request.ServerVariables.Get("Remote_Addr").ToString();
                     }
                     var param = new Dictionary<string, object>();
-                    param.Add("userName", session.UserName);
-                    param.Add("subUserName", session.SubUserName);
+                    param.Add("userName", session.fUserName);
+                    param.Add("subUserName", session.fSubUserName);
                     param.Add("userId", "");
                     param.Add("isPoxy", "0");
-                    param.Add("session", session.TopSessions);
+                    param.Add("session", session.fSession);
                     param.Add("Ip", ip);
                     param.Add("Versions", "1.0");
-                    param.Add("loginUrl", session.LoginUrl);
+                    param.Add("loginUrl", session.fLoginUrl);
                     param.Add("FeeCode", session.FeeCode);
                     // 获取用户信息
                     session = bllUserHandler.UpdateUserInfo(param, false);
                     Session["user"] = session;
-                    return Redirect("http://ao.taokuaiche.com/User/UserLogin?" + session.LoginUrl + "&itemCode=" + session.FeeCode + "&deadLine=" + session.DeadLine);
+                    return Redirect("http://ao.taokuaiche.com/User/UserLogin?" + session.fLoginUrl + "&itemCode=" + session.FeeCode + "&deadLine=" + session.DeadLine);
                 }
                 else if (session.FeeCode == "ts-25420-2")
                 {//专家开车版本
-                    return Redirect("http://ao.taokuaiche.com/User/UserLogin?" + session.LoginUrl + "&itemCode=" + session.FeeCode + "&deadLine=" + session.DeadLine);
+                    return Redirect("http://ao.taokuaiche.com/User/UserLogin?" + session.fLoginUrl + "&itemCode=" + session.FeeCode + "&deadLine=" + session.DeadLine);
                 }
             }
 
